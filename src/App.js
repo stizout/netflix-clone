833369949172e1f61c97167529934beef45c98d9
Alex Stout <stizout@gmail.com>
2024-02-08 12:35:08 -0700

    add plex api

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import './App.css';
 import Header from './components/Header';
 import Row from './components/Row';
 import db from './db.json';
+import { useEffect, useState } from 'react';
+import axios from 'axios';
 
 const movies = [
 	{
@@ -24,6 +26,52 @@ const movies = [
 ];
 
 function App() {
+	const [first, setfirst] = useState('loading');
+	const apiKey = process.env.PLEX_KEY;
+	useEffect(() => {
+		let config = {
+			method: 'get',
+			maxBodyLength: Infinity,
+			url: 'https://192-168-0-2.46da8d4760e7496ab8d52b332ce7c143.plex.direct:32400/library/sections/1/unwatched',
+			// url: 'https://plex.tv/api/v2/pins',
+
+			headers: {
+				Accept: 'application/json',
+				'X-Plex-Token': apiKey,
+				// 'X-Plex-Client-Identifier': 'api',
+			},
+			body: {
+				clientIdentifer: 'api',
+			},
+		};
+		axios(config)
+			.then(({ data }) => {
+				console.log(data.MediaContainer.Metadata);
+				setfirst(`Found ${data.MediaContainer.Metadata.length} movies`);
+			})
+			.catch(() => {
+				setfirst('Error');
+			});
+		// axios(config)
+		// 	.then((response) => {
+		// 		console.log(response.data.id);
+		// 		axios({
+		// 			method: 'get',
+		// 			url: `https://plex.tv/api/v2/pins/${response.data.id}`,
+		// 			headers: {
+		// 				Accept: 'application/json',
+		// 				'X-Plex-Token': 'NFXXXyX6bbYAqeo9QbcW',
+		// 				'X-Plex-Client-Identifier': 'api',
+		// 			},
+		// 		}).then((r) => {
+		// 			console.log(r.data);
+		// 		});
+		// 	})
+		// 	.catch((e) => {
+		// 		console.log(e);
+		// 	});
+	}, []);
+
 	const updated = movies.map((m) => {
 		const num = Math.floor(Math.random() * 1000);
 		return {
@@ -34,6 +82,7 @@ function App() {
 	return (
 		<div className='App bg-neutral-950'>
 			<Header />
+			<p className='text-white'>{first}</p>
 			<div className='overflow-hidden'>
 				{updated.map((m, i) => (
 					<Row
